014faebc121d721cfdb90063ec1cee24f200c4f2
Vikram Kumar <vk8969338090@gmail.com>
2024-01-22 08:41:50 +0530

    some new projects added

---
+++ main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        String s = "Thi's is a sentence";
+        String[] words = s.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        String ans = "";
+
+        for (String word : words) {
+            ans += sb.append(word);
+        }
+        System.out.println(ans);
+    }
+
+}
