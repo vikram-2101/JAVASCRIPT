f6f8ff7df0039d1a6b5ebb64b62fc0a39af65fe8
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2024-01-25 17:49:14 +0530

    Update script.js

--- Code/script.js
+++ Code/script.js
@@ -1,4 +1,4 @@
-// let modebtn = document.querySelector("#mode");
+s// let modebtn = document.querySelector("#mode");
 // let currMode = "light";
 // let body = document.querySelector("body");
 // modebtn.addEventListener("click", () => {
